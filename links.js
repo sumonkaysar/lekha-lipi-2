b1e21f0d89aa30be090b89d25a234d5b97fb4002
sumonkaysar <msk.sumon.kaysar@gmail.com>
2024-06-18 22:53:20 +0600

    changes

--- links.js
+++ links.js
@@ -1,3 +1,3 @@
-export const server = "http://localhost:5000";
-// export const server = "https://lekha-lipi-server.onrender.com";
+// export const server = "http://localhost:5000";
+export const server = "https://lekha-lipi-server.onrender.com";
 export const imgbbUrl = `https://api.imgbb.com/1/upload?key=${import.meta.env.VITE_imgbbKey}`;
